794582fd4cff7a6f5e48c491cc0200af7b5851c0
Thibault Charbonnier <thibaultcha@me.com>
2015-04-25 21:31:35 +0200

    add pcre dependency to Kong Formula

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -4,6 +4,7 @@ class Kong < Formula
   version "0.1.1beta"
   sha256 "6abfea43c705b58e3700b60e4e0bffd529a0bb89ecb311a532d788520f27c70c"
 
+  depends_on 'pcre'
   depends_on 'openssl'
   depends_on 'mashape/kong/ngx_openresty'
   depends_on 'naartjie/luajit/luarocks-luajit'
