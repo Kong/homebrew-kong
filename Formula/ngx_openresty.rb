b686655fe00fbae5786af18eb3785f76b220415c
Thibault Charbonnier <thibaultcha@me.com>
2015-07-21 15:36:09 -0700

    Update ngx_openresty shasum to 256

--- Formula/ngx_openresty.rb
+++ Formula/ngx_openresty.rb
@@ -5,7 +5,7 @@ class NgxOpenresty < Formula
 
   stable do
     url "http://openresty.org/download/ngx_openresty-1.7.10.2.tar.gz"
-    sha1 "0cc7a3fe75fbe50dec619af1a09b87f7f8c79e1d"
+    sha256 "5e8beafb7b32ba62fd34b323b2e9cf49884b4f0491fccf189f0b88b3e25dd0e4"
     # Patch to support ssl-cert-by-lua
     # https://github.com/openresty/lua-nginx-module/issues/331#issuecomment-77279170
     patch :DATA
