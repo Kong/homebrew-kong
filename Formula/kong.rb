6e1719773c07eaa5623c4f78161b6c215354546d
Thibault Charbonnier <thibaultcha@me.com>
2015-11-17 12:18:02 -0800

    release: 0.5.3 bis

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -3,7 +3,7 @@ class Kong < Formula
 
   stable do
     url "https://github.com/Mashape/kong/archive/0.5.3.tar.gz"
-    sha256 "abbfe0a3f3d45026672483890a3ffe79279d5cd64a5241edccc01b6c41862cff"
+    sha256 "2e9579c88290dc40519dd760d5b25c7adb5bf499b03c670be038c03864b54998"
   end
 
   head do
