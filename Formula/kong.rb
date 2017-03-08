d5bd6f352e7f5071f1fda294420f0b7f5e1a869d
Thibault Charbonnier <thibaultcha@me.com>
2017-03-07 20:14:33 -0800

    release: 0.10.0

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -1,14 +1,14 @@
 class Kong < Formula
   homepage "https://getkong.org"
-  desc "Open-source Microservice and API Gateway"
+  desc "Open source Microservices and API Gateway"
 
   stable do
-    url "https://github.com/Mashape/kong.git", :tag => "0.9.9"
+    url "https://github.com/Mashape/kong.git", :tag => "0.10.0"
   end
 
-  devel do
-    url "https://github.com/mashape/kong.git", :tag => "0.10.0rc4"
-  end
+  #devel do
+  #  url "https://github.com/mashape/kong.git", :tag => "0.10.0rc4"
+  #end
 
   head do
     url "https://github.com/Mashape/kong.git", :branch => "next"
@@ -19,8 +19,6 @@ class Kong < Formula
   depends_on "serf"
   depends_on "mashape/kong/openresty"
   depends_on "mashape/kong/luarocks"
-
-  conflicts_with "cassandra", :because => "Kong only supports Cassandra 2.1/2.2"
 
   def install
     system "luarocks make OPENSSL_DIR=#{Formula['openssl'].opt_prefix}"
