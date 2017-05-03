0b7c110871406831100e75d205af177779f54957
Thibault Charbonnier <thibaultcha@me.com>
2017-05-03 12:53:19 -0700

    fix(luarocks) don't write to /usr/local anymore
    * define system tree in Cellar instead of /usr/local
    * patch Kong to make it LuaRocks aware and find said system tree
    
    Fix #39

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -19,8 +19,50 @@ class Kong < Formula
   depends_on "mashape/kong/openresty"
   depends_on "mashape/kong/luarocks"
 
+  patch :DATA
+
   def install
     system "luarocks make OPENSSL_DIR=#{Formula['openssl'].opt_prefix}"
     bin.install "bin/kong"
   end
 end
+
+__END__
+diff --git a/bin/busted b/bin/busted
+index 991b418e..a6d9bb72 100755
+--- a/bin/busted
++++ b/bin/busted
+@@ -1,5 +1,7 @@
+ #!/usr/bin/env resty
+ 
++require "luarocks.loader"
++
+ require("kong.core.globalpatches")({
+   cli = true,
+   rbusted = true
+diff --git a/bin/kong b/bin/kong
+index 3311ce22..c144f46d 100755
+--- a/bin/kong
++++ b/bin/kong
+@@ -1,5 +1,7 @@
+ #!/usr/bin/env resty
+ 
++require "luarocks.loader"
++
+ package.path = "?/init.lua;"..package.path
+ 
+ require("kong.cmd.init")(arg)
+diff --git a/kong/kong.lua b/kong/kong.lua
+index 952f8427..66530bf9 100644
+--- a/kong/kong.lua
++++ b/kong/kong.lua
+@@ -24,6 +24,8 @@
+ -- |[[    ]]|
+ -- ==========
+ 
++require "luarocks.loader"
++
+ do
+   -- let's ensure the required shared dictionaries are
+   -- declared via lua_shared_dict in the Nginx conf
+
