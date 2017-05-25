70e34cd5f199e453ebdb27c9941eac4daf454772
Thibault Charbonnier <thibaultcha@me.com>
2017-05-24 23:59:30 -0700

    fix(kong) remove luarocks.loader patch
    This patch is included in the 0.10.3 release

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -19,49 +19,8 @@ class Kong < Formula
   depends_on "mashape/kong/openresty"
   depends_on "mashape/kong/luarocks"
 
-  patch :DATA
-
   def install
     system "luarocks make OPENSSL_DIR=#{Formula['openssl'].opt_prefix}"
     bin.install "bin/kong"
   end
 end
-
-__END__
-diff --git a/bin/busted b/bin/busted
-index 991b418e..a6d9bb72 100755
---- a/bin/busted
-+++ b/bin/busted
-@@ -1,5 +1,7 @@
- #!/usr/bin/env resty
- 
-+require "luarocks.loader"
-+
- require("kong.core.globalpatches")({
-   cli = true,
-   rbusted = true
-diff --git a/bin/kong b/bin/kong
-index 3311ce22..c144f46d 100755
---- a/bin/kong
-+++ b/bin/kong
-@@ -1,5 +1,7 @@
- #!/usr/bin/env resty
- 
-+require "luarocks.loader"
-+
- package.path = "?/init.lua;"..package.path
- 
- require("kong.cmd.init")(arg)
-diff --git a/kong/templates/nginx_kong.lua b/kong/templates/nginx_kong.lua
-index 536dad9a..66a59f85 100644
---- a/kong/templates/nginx_kong.lua
-+++ b/kong/templates/nginx_kong.lua
-@@ -45,6 +45,7 @@ lua_ssl_verify_depth ${{LUA_SSL_VERIFY_DEPTH}};
- > end
- 
- init_by_lua_block {
-+    require "luarocks.loader"
-     require 'resty.core'
-     kong = require 'kong'
-     kong.init()
-
