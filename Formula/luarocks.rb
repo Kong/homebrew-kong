0a7eb9c6ee82a55e8bb039162baf63bd1a2ddef6
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-12-13 02:00:21 +0200

    chore(*) homebrew upgraded for Kong 1.0.0

--- Formula/luarocks.rb
+++ Formula/luarocks.rb
@@ -1,23 +1,24 @@
 class Luarocks < Formula
+  desc "Package manager for the Lua programming language"
   homepage "https://luarocks.org"
-  url "https://luarocks.org/releases/luarocks-2.4.2.tar.gz"
-  sha256 "0e1ec34583e1b265e0fbafb64c8bd348705ad403fe85967fd05d3a659f74d2e5"
-  head "https://github.com/keplerproject/luarocks.git"
-
-  patch :DATA
+  url "https://luarocks.org/releases/luarocks-3.0.4.tar.gz"
+  sha256 "1236a307ca5c556c4fed9fdbd35a7e0e80ccf063024becc8c3bf212f37ff0edf"
+  head "https://github.com/luarocks/luarocks.git"
 
   depends_on "kong/kong/openresty"
 
+  patch :DATA
+
   def install
-    openresty = Formula["kong/kong/openresty"]
+    openresty_prefix = Formula["kong/kong/openresty"].prefix
 
     # Install to the Cellar, but the tree to install modules is in HOMEBREW_PREFIX/opt/kong
     args = [
       "--prefix=#{prefix}",
       "--rocks-tree=#{HOMEBREW_PREFIX}/opt/kong",
       "--sysconfdir=#{prefix}/etc",
-      "--with-lua=#{openresty.prefix}/luajit",
-      "--with-lua-include=#{openresty.prefix}/luajit/include/luajit-2.1",
+      "--with-lua=#{openresty_prefix}/luajit",
+      "--with-lua-include=#{openresty_prefix}/luajit/include/luajit-2.1",
       "--lua-suffix=jit"
     ]
 
@@ -35,14 +36,14 @@ end
 # Do not attempt to create the rocks tree prefix directory,
 # since /usr/local/opt/kong lies outside of our sandbox.
 __END__
-diff -u a/luarocks-2.4.2/Makefile b/luarocks-2.4.2/Makefile
---- luarocks-2.4.2/Makefile	2016-11-30 04:49:34.000000000 -0800
-+++ luarocks-2.4.2-new/Makefile	2017-08-03 14:54:45.000000000 -0700
-@@ -133,7 +133,6 @@
- 	cp src/luarocks/site_config.lua "$(DESTDIR)$(LUADIR)/luarocks"
- 
- write_sysconfig:
--	mkdir -p "$(DESTDIR)$(ROCKS_TREE)"
- 	if [ ! -f "$(DESTDIR)$(CONFIG_FILE)" ] ;\
- 	then \
- 	   mkdir -p `dirname "$(DESTDIR)$(CONFIG_FILE)"` ;\
+diff -u a/luarocks-3.0.4/GNUmakefile b/luarocks-3.0.4/GNUmakefile
+--- luarocks-3.0.4/GNUmakefile	2018-10-30 19:31:39.000000000 +0200
++++ luarocks-3.0.4-new/GNUmakefile	2018-12-19 02:20:03.000000000 +0200
+@@ -146,7 +146,6 @@
+ # ----------------------------------------
+
+ bootstrap: luarocks $(DESTDIR)$(luarocksconfdir)/config-$(LUA_VERSION).lua
+-	./luarocks make --tree="$(DESTDIR)$(rocks_tree)"
+
+ # ----------------------------------------
+ # Windows binary build
