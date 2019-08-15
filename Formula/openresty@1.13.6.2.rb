34a1c6b93e9ab836e8b7fda92722c7cb749dc8d7
Hisham Muhammad <hisham@gobolinux.org>
2019-08-15 10:31:07 -0300

    fix(openresty) fetch all of openresty-build-tools, not just the script

--- Formula/openresty@1.13.6.2.rb
+++ Formula/openresty@1.13.6.2.rb
@@ -2,17 +2,13 @@ class OpenrestyAT11362 < Formula
   desc "Scalable Web Platform by Extending Nginx with Lua"
   homepage "https://openresty.org/"
   version "1.13.6.2"
-  kong_ngx_build_version = "0.0.5"
-  kong_ngx_build_sha_sum = "9ed2a6468ecfc976d8f3468ce6a138a05f375a2b65e858183f90d84f761bca34"
 
   stable do
-    url "https://raw.githubusercontent.com/Kong/openresty-build-tools/#{kong_ngx_build_version}/kong-ngx-build"
-    sha256 kong_ngx_build_sha_sum
+    url "https://github.com/Kong/openresty-build-tools.git", :tag => "0.0.6"
   end
 
   head do
-    url "https://raw.githubusercontent.com/Kong/openresty-build-tools/#{kong_ngx_build_version}/kong-ngx-build"
-    sha256 kong_ngx_build_sha_sum
+    url "https://github.com/Kong/openresty-build-tools.git"
   end
 
   option "with-debug", "Compile with support for debug logging but without proper gdb debugging symbols"
@@ -22,8 +18,6 @@ class OpenrestyAT11362 < Formula
   conflicts_with "kong/kong/luarocks", :because => "We switched over to a new build method and LuaRocks no longer needs to be installed separately. Please remove it with \"brew remove kong/kong/luarocks\"."
 
   def install
-    chmod 0755, "#{pwd}/kong-ngx-build"
-
     args = [
       "--prefix #{prefix}",
       "--openresty #{version}",
