59653b66bb8f1146e4f8f3402f9965ff1c95339c
Vinicius Mignot <vinicius.mignot@gmail.com>
2021-04-14 09:57:56 -0300

    chore(kong) bump kong to 2.4.0 (#165)

---
+++ Formula/openresty@1.19.3.1.rb
@@ -0,0 +1,54 @@
+class OpenrestyAT11931 < Formula
+  desc "Scalable Web Platform by Extending Nginx with Lua"
+  homepage "https://openresty.org/"
+  KONG_BUILD_TOOLS_VERSION = "4.17.0"
+  KONG_BUILD_TOOLS_SHA_SUM = "5a3d6f0c6e65c97c9a12cbe3eb0380b87f465e83d95f997acab97e15c5a4648b"
+  OPENRESTY_VERSION = "1.19.3.1"
+  OPENSSL_VERSION = "1.1.1k"
+  LUAROCKS_VERSION = "3.5.0"
+  PCRE_VERSION = "8.44"
+
+  version OPENRESTY_VERSION
+
+  url "https://github.com/Kong/kong-build-tools/archive/#{KONG_BUILD_TOOLS_VERSION}.zip"
+  sha256 KONG_BUILD_TOOLS_SHA_SUM
+
+  option "with-debug", "Compile with support for debug logging but without proper gdb debugging symbols"
+
+  depends_on "coreutils"
+  conflicts_with "kong/kong/luarocks", :because => "We switched over to a new build method and LuaRocks no longer needs to be installed separately. Please remove it with \"brew remove kong/kong/luarocks\"."
+
+  def install
+    # LuaJIT build is crashing in macOS Catalina. The defaults
+    # for stack checks changed (they are on by default when the
+    # target is 10.15). An existing issue in Clang will generate
+    # code that crashes under some circumstances if stack checks
+    # are enabled.
+    # https://forums.developer.apple.com/thread/121887
+    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
+
+    args = [
+      "--prefix #{prefix}",
+      "--openresty #{OPENRESTY_VERSION}",
+      "--openssl #{OPENSSL_VERSION}",
+      "--luarocks #{LUAROCKS_VERSION}",
+      "--pcre #{PCRE_VERSION}",
+      "-j #{ENV.make_jobs}"
+    ]
+
+    # Debugging mode, unfortunately without debugging symbols
+    if build.with? "debug"
+      args << "--debug"
+
+      opoo "OpenResty and dependencies will be built --with-debug option, but without debugging symbols. For debugging symbols you have to compile it by hand."
+    end
+
+    Dir.chdir('openresty-build-tools')
+    system "./kong-ngx-build", *args
+
+    bin.install_symlink "#{prefix}/openresty/nginx/sbin/nginx"
+    bin.install_symlink "#{prefix}/openresty/bin/openresty"
+    bin.install_symlink "#{prefix}/openresty/bin/resty"
+    bin.install_symlink "#{prefix}/luarocks/bin/luarocks"
+  end
+end
