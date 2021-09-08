baad5113ba21ab111f0cd0c0b7991d730de1c66c
Wangchong Zhou <fffonion@gmail.com>
2021-09-08 23:32:50 +0800

    fix(formula) set libyaml inc and lib directory for building (#177)
    Homebrew uses a different prefix `/opt/homebrew` for all the stuff on Mac with Apple M1,
    this patch set YAML_LIBDIR and YAML_INCDIR dynamically based on installation path of libyaml.
    
    Fix #174

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -25,11 +25,16 @@ class Kong < Formula
     luarocks_prefix = openresty_prefix + "luarocks"
     openssl_prefix = openresty_prefix + "openssl"
 
+    yaml_libdir = Formula["libyaml"].opt_lib
+    yaml_incdir = Formula["libyaml"].opt_include
+
     system "#{luarocks_prefix}/bin/luarocks",
            "--tree=#{prefix}",
            "make",
            "CRYPTO_DIR=#{openssl_prefix}",
-           "OPENSSL_DIR=#{openssl_prefix}"
+           "OPENSSL_DIR=#{openssl_prefix}",
+           "YAML_LIBDIR=#{yaml_libdir}",
+           "YAML_INCDIR=#{yaml_incdir}"
 
     bin.install "bin/kong"
   end
