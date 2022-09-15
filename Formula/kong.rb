0bcfb9d2258043eeac8b4ea75c0edb3e8451dbd7
Isa Farnik <isa@konghq.com>
2022-09-15 14:33:45 -0700

    chore(kong): use HOMEBREW_PREFIX sub in end patch

--- Formula/kong.rb
+++ Formula/kong.rb
@@ -55,25 +55,25 @@ class Kong < Formula
   end
 end
 
-# patch Kong default `prefix` to `HOMEBREW_PREFIX/opt/kong`
-# to ensure it's writeable
-# additionally, add brew on m1 paths to lua_path and lua_cpath
 __END__
 diff --git a/bin/kong b/bin/kong
-index 3e0ecf97d..b03e18a23 100755
 --- a/bin/kong
 +++ b/bin/kong
 @@ -4,6 +4,7 @@ setmetatable(_G, nil)
+#
+# patch bin/kong's LUA_PATH & LUA_CPATH with HOMEBREW_PREFIX (both intel & arm)
+# homebrew substitutes "HOMEBREW_PREFIX" when patching:
+#   https://docs.brew.sh/Formula-Cookbook#patches
+#
  
  pcall(require, "luarocks.loader")
  
 -package.path = (os.getenv("KONG_LUA_PATH_OVERRIDE") or "") .. "./?.lua;./?/init.lua;" .. package.path
-+package.cpath = (os.getenv("KONG_LUA_CPATH_OVERRIDE") or "") .. "/opt/homebrew/lib/lua/5.1/?.so;" .. package.cpath
-+package.path = (os.getenv("KONG_LUA_PATH_OVERRIDE") or "") .. "./?.lua;./?/init.lua;" .. "/opt/homebrew/share/lua/5.1/?.lua;/opt/homebrew/share/lua/5.1/?/init.lua;" .. package.path
++package.cpath = (os.getenv("KONG_LUA_CPATH_OVERRIDE") or "") .. "HOMEBREW_PREFIX/lib/lua/5.1/?.so;" .. package.cpath
++package.path = (os.getenv("KONG_LUA_PATH_OVERRIDE") or "") .. "./?.lua;./?/init.lua;" .. "HOMEBREW_PREFIX/share/lua/5.1/?.lua;HOMEBREW_PREFIX/share/lua/5.1/?/init.lua;" .. package.path
  
  require("kong.cmd.init")(arg)
 diff --git a/kong/templates/kong_defaults.lua b/kong/templates/kong_defaults.lua
-index 5937dad10..c3387fded 100644
 --- a/kong/templates/kong_defaults.lua
 +++ b/kong/templates/kong_defaults.lua
 @@ -1,5 +1,5 @@
@@ -89,8 +89,8 @@ index 5937dad10..c3387fded 100644
  lua_ssl_protocols = TLSv1.1 TLSv1.2 TLSv1.3
 -lua_package_path = ./?.lua;./?/init.lua;
 -lua_package_cpath = NONE
-+lua_package_path = ./?.lua;./?/init.lua;/opt/homebrew/share/lua/5.1/?.lua;/opt/homebrew/share/lua/5.1/?/init.lua;;
-+lua_package_cpath = /opt/homebrew/lib/lua/5.1/?.so;;
++lua_package_path = ./?.lua;./?/init.lua;HOMEBREW_PREFIX/share/lua/5.1/?.lua;HOMEBREW_PREFIX/share/lua/5.1/?/init.lua;;
++lua_package_cpath = HOMEBREW_PREFIX/lib/lua/5.1/?.so;;
  
  role = traditional
  kic = off
