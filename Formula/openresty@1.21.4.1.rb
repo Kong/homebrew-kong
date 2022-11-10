ff3dd5d39058f00024b61e5145282ee3e9da7332
Ben Doerr <ben.doerr@luminsmart.com>
2022-11-10 14:47:46 -0500

    Fix KONG_BUILD_TOOLS_SHA_SUM for openresty 1.21.4.1
    I went to `brew install kong` today and ran into an issue where `kong-build-tools-4.33.22.tar.gz` is computing to a different sum than the one that is specified. I confirmed directly downloading the archive from github and running `sha256sum` on it.

--- Formula/openresty@1.21.4.1.rb
+++ Formula/openresty@1.21.4.1.rb
@@ -2,7 +2,7 @@ class OpenrestyAT12141 < Formula
   desc "Scalable Web Platform by Extending Nginx with Lua"
   homepage "https://openresty.org/"
   KONG_BUILD_TOOLS_VERSION = "4.33.22".freeze
-  KONG_BUILD_TOOLS_SHA_SUM = "848452e07cc5d4bff73f3d4c0ac12dcb63ede89029b5e9dec5eab1fe28fbe5c8".freeze
+  KONG_BUILD_TOOLS_SHA_SUM = "9397eb077960af5205f31b5a85025bc5262aee47054b4a797e569553ba3d8371".freeze
   OPENRESTY_VERSION = "1.21.4.1".freeze
   OPENSSL_VERSION = "1.1.1q".freeze
   LUAROCKS_VERSION = "3.9.1".freeze
